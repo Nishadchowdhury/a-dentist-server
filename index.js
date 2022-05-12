b8aa8f10013517ff7901aaa83af037b63a75e155
Nishadchowdhury <Nishadhj111@gmail.com>
2022-05-12 15:22:44 +0600

    get all data api add

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const app = express();
-const port = process.env.PORT || 5000;
+const { a, b } = { x: 2, y: 3 }
 
-app.use(cors());
-app.use(express.json());
-
-
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World! from Doctors app')
-})
-
-app.listen(port, () => {
-    console.log(`Doctors app listening on port${port}`)
-})
+console.log(b);
